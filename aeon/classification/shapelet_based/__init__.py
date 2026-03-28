c75367e367b21102113f6ad6413e99688ccbaa7c
Billy Thorne <bt2g23@soton.ac.uk>
2026-03-28 11:34:34 +0000

    Multiple pipeline experiments

--- aeon/classification/shapelet_based/__init__.py
+++ aeon/classification/shapelet_based/__init__.py
@@ -6,6 +6,11 @@ __all__ = [
     "RSTClassifier",
     "RDSTClassifier_rotation_only",
     "RDSTClassifier_rotation_pipeline",
+    "RDSTClassifier_rotation_pipeline_2",
+    "RDSTClassifier_rotation_pipeline_3",
+    "RDSTClassifier_rotation_pipeline_4",
+    "RDSTClassifier_rotation_pipeline_pca_95",
+    "RDSTClassifier_rotation_pipeline_pca_99",
     "SASTClassifier",
     "RSASTClassifier",
     "LearningShapeletClassifier",
@@ -17,5 +22,10 @@ from aeon.classification.shapelet_based._rsast import RSASTClassifier
 from aeon.classification.shapelet_based._rstc import RSTClassifier
 from aeon.classification.shapelet_based._sast import SASTClassifier
 from aeon.classification.shapelet_based._rdst_rotation_pipeline import RDSTClassifier_rotation_pipeline
+from aeon.classification.shapelet_based._rdst_rotation_pipeline_2 import RDSTClassifier_rotation_pipeline_2
+from aeon.classification.shapelet_based._rdst_rotation_pipeline_3 import RDSTClassifier_rotation_pipeline_3
+from aeon.classification.shapelet_based._rdst_rotation_pipeline_4 import RDSTClassifier_rotation_pipeline_4
+from aeon.classification.shapelet_based._rdst_rotation_pipeline_pca_95 import RDSTClassifier_rotation_pipeline_pca_95
+from aeon.classification.shapelet_based._rdst_rotation_pipeline_pca_99 import RDSTClassifier_rotation_pipeline_pca_99
 from aeon.classification.shapelet_based._rdst_rotation_only import RDSTClassifier_rotation_only
 from aeon.classification.shapelet_based._stc import ShapeletTransformClassifier
